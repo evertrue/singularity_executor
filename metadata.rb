2f1c1c656b78a87599929b7d40fffba10b0af5d0
Eric Herot <eric.github@herot.com>
2017-07-26 11:06:28 -0400

    Version bump 2.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs Singularity Executor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.2'
+version          '2.1.3'
 
 supports 'ubuntu', '>= 14.04'
 
